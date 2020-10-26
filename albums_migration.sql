1fea30e4bdc527f8c554ff87da76eca2ac08f4da
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-10-26 16:02:36 -0500

    finished exerc
    ;
    ;
    .
    '

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,11 +1,11 @@
 USE codeup_test_db;
-DROP TABLE IF EXISTS album;
+DROP TABLE IF EXISTS albums;
 CREATE TABLE albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(50),
-    name  VARCHAR(100) NOT NULL,
-    release_date VARCHAR(50),
-    sales  VARCHAR(100) NOT NULL,
-    genre TEXT NOT NULL,
+    artist VARCHAR(50) NOT NULL,
+    name  VARCHAR(50) NOT NULL DEFAULT 'untitled',
+    release_date INT UNSIGNED,
+    sales  FLOAT UNSIGNED,
+    genre VARCHAR (20)NOT NULL,
     PRIMARY KEY (id)
 )
