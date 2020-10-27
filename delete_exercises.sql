f6fbdf256b9e5fd7215df72ef9323db4ee2659e9
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-10-27 14:43:57 -0500

    adjusted commented out data

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,12 +1,12 @@
 USE codeup_test_db;
--- SELECT 'Albums released after 1991' AS 'Exercise';
--- DELETE FROM albums WHERE release_date > 1991;
--- SELECT * FROM albums;
+SELECT 'Albums released after 1991' AS 'Exercise';
+DELETE FROM albums WHERE release_date > 1991;
+SELECT * FROM albums;
 
--- SELECT 'Albums with the genre ''disco''' AS 'Exercise';
--- DELETE FROM albums WHERE genre =  'disco';
---
--- SELECT * FROM albums;
+SELECT 'Albums with the genre ''disco''' AS 'Exercise';
+DELETE FROM albums WHERE genre =  'disco';
+
+SELECT * FROM albums;
 
 
 SELECT 'Albums by ''Whitney Houston'' (...or maybe an artist of your choice)' AS 'Exercise';
--- update_exercises.sql
+++ update_exercises.sql
@@ -3,10 +3,10 @@ SELECT 'All albums in your table.' AS 'Exercise';
 SELECT * FROM albums;
 
 
--- SELECT 'Make all the albums 10 times more popular' AS 'Exercise';
--- UPDATE albums
--- SET sales = sales*10;
--- SELECT * FROM albums;
+SELECT 'Make all the albums 10 times more popular' AS 'Exercise';
+UPDATE albums
+SET sales = sales*10;
+SELECT * FROM albums;
 
 
 
@@ -14,10 +14,10 @@ SELECT 'All albums released before 1980' AS 'Exercise';
 SELECT * FROM albums WHERE release_date < 1980;
 
 SELECT 'Move all the albums before 1980 back to the 1800s.' AS 'Exercise';
--- UPDATE albums
--- SET release_date = release_date - 100
--- WHERE release_date < 1980;
--- SELECT * FROM albums;
+UPDATE albums
+SET release_date = (release_date - 100)
+WHERE release_date < 1980;
+SELECT * FROM albums;
 
 
 SELECT 'Change ''Michael Jackson'' to ''Peter Jackson''' AS 'Exercise';
