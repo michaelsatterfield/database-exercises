0b173138a7f27a7be5eb7b36af1dc408de64e312
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-10-27 11:40:11 -0500

    finished select exercises

---
+++ select_exercises.sql
@@ -0,0 +1,18 @@
+USE codeup_test_db;
+SELECT 'The name of all albums by Pink Floyd.' AS 'Exercise';
+SELECT * FROM albums WHERE artist = 'Pink Floyd';
+
+SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released' AS 'Exercise';
+SELECT release_date FROM albums WHERE album_name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+SELECT 'The genre for Nevermind' AS 'Exercise';
+SELECT genre FROM albums WHERE album_name = 'Nevermind';
+
+SELECT 'Which albums were released in the 1990s' AS 'Exercise';
+SELECT * FROM albums WHERE release_date BETWEEN '1990' AND '2000';
+
+SELECT 'Which albums had less than 20 million certified sales' AS 'Exercise';
+SELECT * FROM albums WHERE sales < '20';
+
+SELECT 'All the albums with a genre of "Rock"' AS 'Exercise';
+SELECT * FROM albums WHERE genre = 'Rock';
