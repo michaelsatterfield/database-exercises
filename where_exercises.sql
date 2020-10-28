d7a54ff4a9bea7cc98b39bd43a6f53309b991e5d
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-10-28 12:54:36 -0500

    finished where,limit exercises

---
+++ where_exercises.sql
@@ -0,0 +1,52 @@
+USE employees;
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%';
+
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '2000-01-01';
+
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%12-25%';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q';
+
+SELECT *
+FROM employees
+WHERE first_name ='Irena'
+   OR  first_name = 'Vidya'
+   OR  first_name ='Maya';
+
+
+
+SELECT *
+FROM employees
+WHERE (first_name ='Irena'
+   OR first_name ='Vidya'
+   OR first_name ='Maya')
+  AND  gender = 'm';
+
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'e%'
+OR last_name LIKE '%e';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'e%'
+AND last_name LIKE '%e';
+
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '2000-01-01'
+AND birth_date LIKE '%12-25%';
