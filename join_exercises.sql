2008e6cc0aa65e93d7e513204f0ac65e98db8692
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-10-29 16:56:29 -0500

    third almost task complete

--- join_exercises.sql
+++ join_exercises.sql
@@ -19,3 +19,27 @@ FROM employees as e
          JOIN departments as d
               ON d.dept_no = dm.dept_no
 WHERE dm.to_date = '9999-01-01'AND e.gender = 'F' ORDER BY dept_name;
+
+
+# Find the current titles of employees currently working in the Customer Service department.
+# SELECT  t.title AS 'Title', count() AS Count From employees
+# FROM employees as e
+#          JOIN titles as t
+#               ON t.emp_no = e.emp_no
+#          JOIN departments as d
+# #               ON d.= e.
+# WHERE t.to_date = '9999-01-01'AND d.dept_name = 'Customer Service' ORDER BY dept_name;
+
+# Find the current salary of all current managers.
+SELECT  d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name ) AS 'Name',s.emp_no AS 'Salary'
+FROM employees as e
+
+         JOIN dept_manager as dm
+              ON dm.emp_no = e.emp_no
+         JOIN departments as d
+              ON d.dept_no = dm.dept_no
+         JOIN salaries as s
+              ON s.salary= e.emp_no
+
+
+WHERE dm.to_date = '9999-01-01' ORDER BY dept_name;
