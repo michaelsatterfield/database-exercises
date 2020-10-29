504604df6a098b34e27ebdfe1fce51588fc2f0cc
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-10-29 09:46:19 -0500

    completed limit exercises

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -4,3 +4,18 @@ SELECT DISTINCT last_name
 FROM employees
 ORDER BY last_name DESC
 LIMIT 10;
+
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '2000-01-01'
+  AND birth_date LIKE '%12-25%'
+ORDER BY  birth_date, hire_date DESC
+LIMIT 5;
+
+
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '2000-01-01'
+  AND birth_date LIKE '%12-25%'
+ORDER BY  birth_date, hire_date DESC
+LIMIT 5 OFFSET  45;
